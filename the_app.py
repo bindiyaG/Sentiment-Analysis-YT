1f7485d0ab86c7d405a914ac19ff00ae7884fcd9
Bindiya Gandhi <101421289+bindiyaG@users.noreply.github.com>
2023-08-10 20:52:41 -0400

    Update the_app.py

--- the_app.py
+++ the_app.py
@@ -9,18 +9,20 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 
-nltk.download('stopwords')
+nltk.download("stopwords")
 
 # Load the pre-trained LSTM model
-model = load_model('lstm_model.h5')
+model = load_model("lstm_model.h5")
+
 
 # Function to clean and filter the text
 def clean_text(text):
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(stopwords.words("english"))
     text = text.lower()
-    text = ''.join([c for c in text if c not in string.punctuation])
-    text = ' '.join([word for word in text.split() if word not in stop_words])
+    text = "".join([c for c in text if c not in string.punctuation])
+    text = " ".join([word for word in text.split() if word not in stop_words])
     return text
+
 
 def prepare_data(texts):
     # Initialize a tokenizer
@@ -32,34 +34,49 @@ def prepare_data(texts):
     sequences = pad_sequences(sequences, maxlen=1408)
     return sequences
 
+
 # Streamlit App
 def main():
     st.title("Sentiment Analysis Web App for Yeti Analytics")
     st.sidebar.title("Settings")
-    
+
     st.markdown("## Enter Your Review")
     user_input = st.text_area("")
 
-    if st.button('Analyze'):
+    if st.button("Analyze"):
         if user_input is not None:
             # Preprocess the review
             cleaned_text = clean_text(user_input)
-            
+
             # Prepare the review for the model
             X = prepare_data([cleaned_text])
 
             # Perform sentiment analysis using the loaded model
             predictions = model.predict(X)
-            
+
             sentiment_score = predictions[0]
             sentiment_label = "Positive" if sentiment_score >= 0.5 else "Negative"
 
             # Display review and sentiment analysis results
             st.markdown("## Sentiment Analysis Result")
-            st.table(pd.DataFrame({"Review": [user_input], "Sentiment Label": [sentiment_label]}))
+            st.table(
+                pd.DataFrame(
+                    {"Review": [user_input], "Sentiment Label": [sentiment_label]}
+                )
+            )
 
             st.markdown("## Predicted Sentiment Label")
-            st.bar_chart(pd.DataFrame({'Labels': ['Positive', 'Negative'], 'Count': [sentiment_score, 1-sentiment_score]}).set_index('Labels'))
+            # Create a DataFrame with sentiment labels and their counts
+            sentiment_data = pd.DataFrame(
+                {
+                    "Labels": ["Positive", "Negative"],
+                    "Count": [int(sentiment_score >= 0.5), int(sentiment_score < 0.5)],
+                }
+            )
+
+            # Plot the bar chart
+            st.bar_chart(sentiment_data.set_index("Labels"))
+
 
 if __name__ == "__main__":
     main()
